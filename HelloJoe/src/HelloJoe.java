6a1c29e583227ae67623822c58f771aa0741ca83
Gage Farmer <trickyok@users.noreply.github.com>
2020-01-22 11:26:15 -0500

    Joe is sentient

--- HelloJoe/src/HelloJoe.java
+++ HelloJoe/src/HelloJoe.java
@@ -4,6 +4,8 @@ public class HelloJoe {
 
 	public static void main(String[] args) {
 		
+		Runtime runtime = Runtime.getRuntime();
+		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Don't ask who Joe is...");
 		String joe = input.nextLine();
@@ -12,6 +14,18 @@ public class HelloJoe {
 		
 		if (joe.startsWith("who") && joe.contains("joe")) {
 			System.out.print("Joe Mama");
+			for (int i = 0; i < 10;) {
+				try
+		        {
+		            runtime.exec("notepad.exe");
+		 
+		            //OR runtime.exec("notepad");
+		        }
+		        catch (Exception e)
+		        {
+		            e.printStackTrace();
+		        }
+			}
 		}
 		
 	}
